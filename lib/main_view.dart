149784de9df67d42f015e38cecbce520899802ce
Dae Hyeon <daehyeon1999@naver.com>
2022-08-26 12:42:24 +0900

    Add video_player package

--- lib/main_view.dart
+++ lib/main_view.dart
@@ -1,8 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_study/model/video_model.dart';
+import 'package:video_player/video_player.dart';
 
-class VideoPlayer extends StatelessWidget {
+class VideoPlayer extends StatefulWidget {
   const VideoPlayer({Key? key}) : super(key: key);
+
+  @override
+  State<VideoPlayer> createState() => _VideoPlayerState();
+}
+
+class _VideoPlayerState extends State<VideoPlayer> {
+  late VideoPlayerController _videoController;
+  final int _currentIndex = 0;
+
+  // initial the video is index 0, and it's not initialize yet
+  void _playVideo({int index = 0, bool init = false}) {
+    // if the index is lowwer of higher than the index, return null
+    if (index < 0 || index > videos.length) return;
+
+    // load the video from the network to the controller
+    _videoController = VideoPlayerController.network(videos[_currentIndex].url)
+      // add a listener
+      ..addListener(() {
+        // update the widget whenever the function called
+        setState(() {});
+      })
+      // make the video loop
+      ..setLooping(true)
+      // play the video after initialize
+      ..initialize().then((value) => _videoController.play());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // initialize
+    _playVideo(init: true);
+  }
 
   @override
   Widget build(BuildContext context) {
