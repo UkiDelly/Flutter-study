e9fb807d77b1e39cbfce9d337b6fa57f3d9c3191
Dae Hyeon <daehyeon1999@naver.com>
2022-08-26 12:28:55 +0900

    Show thumnail and title

--- lib/main_view.dart
+++ lib/main_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_study/model/video_model.dart';
 
 class VideoPlayer extends StatelessWidget {
   const VideoPlayer({Key? key}) : super(key: key);
@@ -8,10 +7,44 @@ class VideoPlayer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+        appBar: AppBar(
+          title: const Text("Video Player"),
+        ),
 
-      //
-      body: const SizedBox(),
-    );
+        //
+        body: Column(
+          children: [
+            // show video list
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: videos.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Row(
+                    children: [
+                      //* show thumbnail, but thumbnail is not avail
+                      const SizedBox(
+                        width: 100,
+                        height: 100,
+                      ),
+
+                      //
+                      const SizedBox(
+                        width: 20,
+                      ),
+
+                      //* video name
+                      Text(
+                        videos[index].name,
+                        style: const TextStyle(fontSize: 25),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ));
   }
 }
