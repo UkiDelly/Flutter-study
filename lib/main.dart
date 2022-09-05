c56fcd92c6754ca5b1bf620b4464c4a20fb9af92
Dae Hyeon <daehyeon1999@naver.com>
2022-09-05 20:05:26 +0900

    GetX app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_study/personal_card.dart';
 
-import 'package:get/get.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -13,26 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // Reactive State managment을 위해서는 GetMaterialApp을 써야한다.
-    return const GetMaterialApp(title: 'Flutter study', home: PersonalCard());
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('GetX')),
-
-      //
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [],
-      )),
-    );
+    return MaterialApp(title: 'Flutter study', home: Container());
   }
 }
