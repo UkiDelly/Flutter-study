8805e4b748242bf09ee620059ae41d2d2686eefb
Dae Hyeon <daehyeon1999@naver.com>
2022-09-07 14:28:10 +0900

    define colors

---
+++ lib/common/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// 주 색상
+const primaryColor = Color(0xff22A45D);
+
+// 글자 색상
+const bodyTextColor = Color(0xff868686);
+
+// TextFeild 배경 색상
+const inputBGColor = Color(0xfffbfbfb);
+
+// TextFeild 테두리 색상
+const inputBorderColor = Color(0xfff3f2f2);
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const _MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _MyApp extends StatelessWidget {
+  const _MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Flutter study', home: HomePage());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Container(),
+        ));
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
-  }
-}
 
 // Server Repo
 // "https://github.com/codefactory-co/flutter-lv2-server"
