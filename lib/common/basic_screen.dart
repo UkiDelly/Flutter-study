431dc70455a0754d68f86f95ab3aae80712816f7
Dae Hyeon <daehyeon1999@naver.com>
2022-09-10 21:37:57 +0900

    create AppBar and BottomNavigationBar

--- lib/common/basic_screen.dart
+++ lib/common/basic_screen.dart
@@ -3,14 +3,33 @@ import 'package:flutter/material.dart';
 class BasicScreen extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
-  const BasicScreen({super.key, required this.child, this.backgroundColor});
+  final String? title;
+  final BottomNavigationBar? bottomNavigationBar;
+  const BasicScreen({super.key, required this.child, this.backgroundColor, this.title, this.bottomNavigationBar});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // resizeToAvoidBottomInset: false,
+      appBar: renderAppBar(),
       backgroundColor: backgroundColor ?? Colors.white,
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        foregroundColor: Colors.black,
+        title: Text(
+          title!,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+      );
+    }
   }
 }
--- lib/common/root_tab.dart
+++ lib/common/root_tab.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_study/common/basic_screen.dart';
+import 'package:flutter_study/common/colors.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 0;
+  @override
   Widget build(BuildContext context) {
     return BasicScreen(
-        child: Center(
-      child: Text("Root tab"),
-    ));
+      title: "코팩 딜리버리",
+      child: Center(
+        child: Text("Root tab"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: primaryColor,
+        unselectedItemColor: bodyTextColor,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) => setState(() => this.index = index),
+        currentIndex: index,
+        showUnselectedLabels: true,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
+          BottomNavigationBarItem(icon: Icon(Icons.fastfood_outlined), label: '음식'),
+          BottomNavigationBarItem(icon: Icon(Icons.receipt_long_outlined), label: '주문'),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: '프로필'),
+        ],
+      ),
+    );
   }
 }
