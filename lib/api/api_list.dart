0edd1a56867078a45452bbf12d2e34e47b91f748
Dae Hyeon <daehyeon1999@naver.com>
2022-11-01 17:44:18 +0900

    create rating and user model

--- lib/api/api_list.dart
+++ lib/api/api_list.dart
@@ -1,7 +1,3 @@
 import 'dart:io';
 
-final api = Platform.isIOS ? "http://localhost:3000" : "http://10.0.2.2: 3000";
-
-class Api {
-  
-}
+final api = Platform.isIOS ? 'http://localhost:3000' : 'http://10.0.2.2: 3000';
---
+++ lib/model/restaurant/rating_model.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter_study/common/utils/data_utils.dart';
+import 'package:flutter_study/model/user/user_model.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'rating_model.g.dart';
+
+@JsonSerializable()
+class RatingModel {
+  final String id;
+  final UserModel user;
+  final int rating;
+  final String content;
+  @JsonKey(fromJson: DataUtils.listPathsToUrls)
+  final List<String> imageUrls;
+
+  RatingModel({
+    required this.id,
+    required this.user,
+    required this.rating,
+    required this.content,
+    required this.imageUrls,
+  });
+
+  factory RatingModel.fromJson(Map<String, dynamic> json) => _$RatingModelFromJson(json);
+}
