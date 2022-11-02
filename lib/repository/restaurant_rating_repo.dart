864488f66d20af62191cc3b33bf3bd02e8d83e8f
Dae Hyeon <daehyeon1999@naver.com>
2022-11-02 10:32:22 +0900

    create rating provider

--- lib/repository/restaurant_rating_repo.dart
+++ lib/repository/restaurant_rating_repo.dart
@@ -1,11 +1,20 @@
 import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_study/common/dio.dart';
 import 'package:flutter_study/model/restaurant/rating_model.dart';
 import 'package:retrofit/http.dart';
 
+import '../api/api_list.dart';
 import '../common/model/cursor_pagination_model.dart';
 import '../common/model/pagination_params.dart';
 
-part 'restaurant_reating_repo.g.dart';
+part 'restaurant_rating_repo.g.dart';
+
+final restaurantRatingRepoProvider = Provider.family<RestaurantRatingRepo, String>((ref, id) {
+  final dio = ref.watch(dioProvider);
+
+  return RestaurantRatingRepo(dio, baseUrl: '$api/restaurant/$id/rating');
+});
 
 // http://api/restaurant/:rid/rating
 @RestApi()
