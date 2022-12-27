3e3f0c3a6246eafa26b0490e56e90ed48dc3833e
Dae Hyeon <daehyeon1999@naver.com>
2022-12-27 11:43:57 +0900

    get version of ios from method channel

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,4 @@
 import UIKit
-import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +6,25 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+      let versionChannel = FlutterMethodChannel(name: "com.example.flutter_study", binaryMessenger: controller.binaryMessenger)
+      
+      versionChannel.setMethodCallHandler({
+          [weak self](call: FlutterMethodCall, result: FlutterResult)-> Void in guard call.method == "getPlatformVersion" else {
+              result(FlutterMethodNotImplemented)
+              return
+          }
+          self?.getPlatformVersion(result: result)
+          
+      })
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    private func getPlatformVersion(result: FlutterResult){
+        let device = UIDevice.current
+        result(device.systemVersion)
+    }
+  
 }
