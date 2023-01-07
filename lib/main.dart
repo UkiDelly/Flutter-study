8fb29f6720b1b73bdc20321d4715aab6df46835a
Dae Hyeon <daehyeon1999@naver.com>
2023-01-07 20:36:42 +0900

    Rive로 Splash Screen 구현해보기

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study/rive_example.dart';
+import 'package:rive_splash_screen/rive_splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +11,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Rive', home: RiveExample());
+    return MaterialApp(
+        title: 'Rive',
+        home: SplashScreen.navigate(
+            name: 'assets/nodegen.riv',
+            next: (_) => const Home(),
+            until: () => Future.delayed(const Duration()),
+            startAnimation: 'appear',
+            backgroundColor: Colors.black));
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text('Rive'),
+      ),
+      body: const Text("body", style: TextStyle(fontSize: 30, color: Colors.white)),
+    );
   }
 }
--- lib/rive_example.dart
+++ lib/rive_example.dart
@@ -9,48 +9,17 @@ class RiveExample extends StatefulWidget {
 }
 
 class _RiveExampleState extends State<RiveExample> {
-  // Rive 파일의 'body_looking, 'hand', 'lights'이름의 애니메이션을 컨트롤하는 컨트롤러
-  List<RiveAnimationController> animationController = [
-    SimpleAnimation('body_looking'),
-    SimpleAnimation('hand'),
-    SimpleAnimation('lights'),
-  ];
-
-  // Bool 타입의 input
-  SMIBool? trigger;
-
-  // state machine을 컨트롤하는 컨트롤서
   late StateMachineController controller;
-
-  // Rive 애니메이션이 생성되면 호출되는 콜백
-  String artboard = 'Tour';
   void onInit(Artboard artboard) {
-    // Rive 애니메이션의 'Tour' artboard에서 'State Machine 1'이란 이름의 state machine을 불러오기
-    final StateMachineController controller =
-        StateMachineController.fromArtboard(artboard, 'State Machine 1') as StateMachineController;
+    controller = StateMachineController.fromArtboard(
+      artboard,
+      'State Machine',
+    ) as StateMachineController;
 
-    // state machine의 애니매이션 실행
-    controller.isActive = true;
-
-    // artboard에 컨트롤러 추가
     artboard.addController(controller);
-
     setState(() {
-      this.controller = controller;
-
-      // controller에서 bool타입의 isActive이란 이름의 input 찾기
-      trigger = controller.findInput<bool>('isActive') as SMIBool;
+      controller.isActive = true;
     });
-  }
-
-  void toggleLight() {
-    if (trigger != null) {
-      print(trigger!.value);
-      setState(() {
-        // bool타입의 input의 값을 반전시키기
-        trigger!.change(!trigger!.value);
-      });
-    }
   }
 
   @override
@@ -59,31 +28,40 @@ class _RiveExampleState extends State<RiveExample> {
   }
 
   @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Rive Example')),
       body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        ElevatedButton(onPressed: () => toggleLight(), child: const Text('Toggle Light')),
-        SizedBox(
-          height: 400,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 400,
+              // https://rive.app/community/3902-8169-pathfinder/ <- 예제 애니매이션
+              child: RiveAnimation.asset(
+                // .riv 파일 가져오기
+                'assets/nodegen.riv',
+                artboard: 'Logo Animation',
+                // riv파일에서 사용할 artboard 이름설정
+                // riv파일에서 사용할 애니메이션 컨트롤러
+                // controllers: [controller],
 
-          // https://rive.app/community/3902-8169-pathfinder/ <- 예제 애니매이션
-          child: RiveAnimation.asset(
-            // .riv 파일 가져오기
-            'assets/example.riv',
-            // riv파일에서 사용할 artboard 이름설정
-            artboard: artboard,
-            // riv파일에서 사용할 애니메이션 컨트롤러
-            // controllers: [...animationController],
+                // riv파일에서 사용할 state machine 가져오기
+                // stateMachines: ['State Machine 1'],
 
-            // riv파일에서 사용할 state machine 가져오기
-            // stateMachines: const ['State Machine 1'],
-            // Rive 위젯이 생성될때 실행될 함수
-            onInit: onInit,
-          ),
+                // Rive 위젯이 생성될때 실행될 함수
+                onInit: onInit,
+              ),
+            ),
+          ],
         ),
-      ])),
+      ),
     );
   }
 }
